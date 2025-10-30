e8fc3696dbf953f462843653e2cc022395eeef04
Augustinho Baumgarten <106928471+AugustinhoBaumgarten@users.noreply.github.com>
2025-10-30 15:15:20 -0300

    aula estrutura de repetição
    do while / while em JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while(c <= 6)
+
+
+
+
+
+/*
+var c = 1
+while(c <= 10){
+    console.log(`passo ${c}`)
+    c++ //c= c+1
+}
+*/ 
+
+
+//console.log('tudo bem?')
