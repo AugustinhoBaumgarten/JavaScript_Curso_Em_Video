bb31727dd9b1ace0a4ca8fdefc2853b590db1360
Augustinho Baumgarten <106928471+AugustinhoBaumgarten@users.noreply.github.com>
2025-10-31 17:09:38 -0300

    aula 14

---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+/*var c =1
+
+do{
+    console.log(c)
+    c++
+}while(c <= 8) */
+
+//------------------------
+console.log('Vai começar')
+for(var c = 1; c <= 8; c++){
+    console.log(c)
+}
+console.log('fim')
