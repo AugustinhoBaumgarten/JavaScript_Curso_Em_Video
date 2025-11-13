c9634d91ea49c7bc6abd909827e1331f55ad5873
Augustinho Baumgarten <106928471+AugustinhoBaumgarten@users.noreply.github.com>
2025-11-13 14:56:37 -0300

    limpeza aumtomatica na barra de input

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,27 +1,61 @@
-var num = document.getElementById(fnum)
-var lista = document.getElementById(flista)
-var res = document.getElementById(res)
+var num = document.getElementById('fnum')
+var lista = document.getElementById('flista')
+var res = document.getElementById('res')
 var valores = []
 
-function isNumero(n){
-    if(Number(n) >=1 && Number(n) <=100){
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true
-    }else{
-        return false
-    }
-}
-function isListas(n, l){
-    if(l.indexOf(number(n) != -1)){
-        return true
-    }else{
+    } else {
         return false
     }
 }
 
-function adicionar(){
-    if(isNumero(num.value) && !inLista(num.value)) {
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
-    }else(
-        alert('Valor invalida ou ja encontrado na lista')
-    )
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+        res.innerHTML = '' // limpa o resultado se quiser mostrar depois
+    } else {
+        alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        alert('Adicione valores antes de finalizarr!')
+    }else{
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for(var pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma/tot
+        res.innerHTML=''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados. </p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos valores temos ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores digitados é ${media}</p>`
+    }
 }
