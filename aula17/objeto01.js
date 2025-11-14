2fb0e8a30bdf9eee8403018b1a492b5a7a3595fc
Augustinho Baumgarten <106928471+AugustinhoBaumgarten@users.noreply.github.com>
2025-11-14 17:29:55 -0300

    OO

---
+++ aula17/objeto01.js
@@ -0,0 +1,10 @@
+let amigo = {
+    nome: 'josé', 
+    sexo: 'm', 
+    peso:'85.4', 
+    engordar(p){ 
+        console.log('engordou')
+        this.peso += P
+    }}
+amigo.engordar(2)
+console.log(typeof amigo)
